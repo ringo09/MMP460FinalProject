021d3807ad501ee43fc162b62740f0d1cf55ceb9
ringo09 <ringo.suke@gmail.com>
2014-12-16 14:25:08 -0500

    fix

--- themes/reverie-child/template/noWidget.php
+++ themes/reverie-child/template/noWidget.php
@@ -5,10 +5,8 @@ Template Name: No Widget
 ?>
 <?php get_header(); ?>
 	<!-- Row for main content area -->
-	<div class="row">
 	<?php /* Start loop */ ?>
 	<?php while (have_posts()) : the_post(); ?>
 		<?php the_content(); ?>
 	<?php endwhile; // End the loop ?>
-	</div>
 <?php get_footer(); ?>
